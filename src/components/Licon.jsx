fca00959c750b42077dc48051c17013a13546d9f
cereceda1991 <cereceda1991@gmail.com>
2023-05-22 01:10:43 -0500

    Update

--- src/components/Licon.jsx
+++ src/components/Licon.jsx
@@ -1,30 +1,63 @@
-import React from 'react'
-import { motion, useScroll } from 'framer-motion'
+// import React from 'react'
+// import { motion, useScroll } from 'framer-motion'
 
-const Licon = ({ reference }) => {
+// const Licon = ({ reference }) => {
+//     const { scrollYProgress } = useScroll({
+//         target: reference,
+//         offset: ["center end", "center center"],
+//     });
+
+
+//     return (
+//         <figure>
+//             <svg
+//                 className='svg__rotate'
+//                 width="75"
+//                 height="75"
+//                 viewBox="0 0 100 100">
+//                 <circle cx="75" cy='50' r="20" className='circle1' />
+//                 <motion.circle
+//                     style={{
+//                         pathLength: scrollYProgress,
+//                     }}
+//                 />
+//                 <circle cx="75" cy='50' r="10" className='circle2' />
+//             </svg>
+//         </figure>
+//     )
+// }
+
+// export default Licon
+
+import React, { useRef } from 'react';
+import { motion, useScroll } from 'framer-motion';
+
+const Licon = () => {
+    const reference = useRef(null);
     const { scrollYProgress } = useScroll({
-        target: reference,
+        target: reference.current,
         offset: ["center end", "center center"],
     });
 
-
     return (
-        <figure>
+        <figure ref={reference}>
             <svg
                 className='svg__rotate'
                 width="75"
                 height="75"
-                viewBox="0 0 100 100">
-                <circle cx="75" cy='50' r="20" className='circle1' />
+                viewBox="0 0 100 100"
+            >
+                <circle cx="75" cy="50" r="20" className='circle1' />
                 <motion.circle
                     style={{
                         pathLength: scrollYProgress,
                     }}
                 />
-                <circle cx="75" cy='50' r="10" className='circle2' />
+                <circle cx="75" cy="50" r="10" className='circle2' />
             </svg>
         </figure>
-    )
-}
+    );
+};
 
-export default Licon
+export default Licon;
+
--- src/utils/playSound.js
+++ src/utils/playSound.js
@@ -1,8 +1,32 @@
-import hoverSound from '../assets/sound.mp3';
+// import hoverSound from '../assets/sound.mp3';
+
+// const playSound = () => {
+//     const audio = new Audio(hoverSound);
+//     audio.play();
+// };
+
+// export default playSound;
+import hoverSound from "../assets/sound.mp3";
 
 const playSound = () => {
+  return new Promise((resolve) => {
     const audio = new Audio(hoverSound);
-    audio.play();
+    const playPromise = audio.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(() => {
+          // Reproducción exitosa
+          resolve();
+        })
+        .catch(() => {
+          // Error en la reproducción, omitir
+          resolve();
+        });
+    } else {
+      // La reproducción no está soportada, omitir
+      resolve();
+    }
+  });
 };
 
 export default playSound;
