eeeeb2dcab26b9bbe7ce457bcbc91744b916980a
cereceda1991 <cereceda1991@gmail.com>
2023-06-23 22:38:00 -0500

    feat: update skill's

--- src/utils/skillInProcess.js
+++ src/utils/skillInProcess.js
@@ -1,23 +1,41 @@
-import { FaVuejs, FaAngular, FaLaravel, FaAws, FaPython } from "react-icons/fa";
-import { SiSvelte, SiFirebase, SiNestjs, SiSwagger, SiMongodb } from "react-icons/si";
-import { DiDjango, DiRuby, DiPhp } from "react-icons/di";
-import { TbBrandReactNative } from "react-icons/tb";
+import { FaVuejs, FaAngular, FaAws, FaPython } from 'react-icons/fa';
+import {
+  SiSvelte,
+  SiFirebase,
+  SiNestjs,
+  SiSwagger,
+  SiMongodb,
+} from 'react-icons/si';
+import { DiDjango, DiRuby } from 'react-icons/di';
+import { TbBrandReactNative } from 'react-icons/tb';
 
 const inProcess = [
-    { name: "Vue.js", icon: FaVuejs, color: "#41B883", category: "Frontend" },
-    { name: "Svelte", icon: SiSvelte, color: "#FF3E00", category: "Frontend" },
-    { name: "Angular", icon: FaAngular, color: "#DD0031", category: "Frontend" },
-    { name: "Swagger", icon: SiSwagger, color: "#85EA2D", category: "Backend" },
-    { name: "Nest.js", icon: SiNestjs, color: "#E0234E", category: "Backend" },
-    { name: "Laravel", icon: FaLaravel, color: "#FF2D20", category: "Backend" },
-    { name: "Django", icon: DiDjango, color: "#092E20", category: "Backend" },
-    { name: "MongoDB", icon: SiMongodb, color: "#4DB33D", category: "Backend" },
-    { name: "Firebase", icon: SiFirebase, color: "#FFA000", category: "Backend" },
-    { name: "AWS", icon: FaAws, color: "#FF9900", category: "Backend" },
-    { name: "React Native", icon: TbBrandReactNative, color: "#61DBFB", category: "Mobile" },
-    { name: "Python", icon: FaPython, color: "#3776AB", category: "Programming Languages" },
-    { name: "Ruby on Rails", icon: DiRuby, color: "#CC0000", category: "Programming Languages" },
-    { name: "PHP", icon: DiPhp, color: "#777BB4", category: "Programming Languages" },
+  { name: 'Vue.js', icon: FaVuejs, color: '#41B883', category: 'Frontend' },
+  { name: 'Svelte', icon: SiSvelte, color: '#FF3E00', category: 'Frontend' },
+  { name: 'Angular', icon: FaAngular, color: '#DD0031', category: 'Frontend' },
+  { name: 'Swagger', icon: SiSwagger, color: '#85EA2D', category: 'Backend' },
+  { name: 'Nest.js', icon: SiNestjs, color: '#E0234E', category: 'Backend' },
+  { name: 'Django', icon: DiDjango, color: '#092E20', category: 'Backend' },
+  { name: 'Firebase', icon: SiFirebase, color: '#FFA000', category: 'Backend' },
+  { name: 'AWS', icon: FaAws, color: '#FF9900', category: 'Backend' },
+  {
+    name: 'React Native',
+    icon: TbBrandReactNative,
+    color: '#61DBFB',
+    category: 'Mobile',
+  },
+  {
+    name: 'Python',
+    icon: FaPython,
+    color: '#3776AB',
+    category: 'Programming Languages',
+  },
+  {
+    name: 'Ruby on Rails',
+    icon: DiRuby,
+    color: '#CC0000',
+    category: 'Programming Languages',
+  },
 ];
 
 export default inProcess;
--- src/utils/skillsData.js
+++ src/utils/skillsData.js
@@ -1,30 +1,105 @@
-import { FaBootstrap, FaCss3Alt, FaDocker, FaFigma, FaHtml5, FaNodeJs, FaNpm, FaReact } from "react-icons/fa";
-import { TbBrandNextjs, TbBrandRedux, TbBrandVite } from "react-icons/tb";
-import { BsGit } from "react-icons/bs";
-import { SiAdobe, SiCanva, SiExpress, SiJavascript, SiPostgresql, SiPostman, SiPowerbi, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+  FaBootstrap,
+  FaCss3Alt,
+  FaDocker,
+  FaFigma,
+  FaHtml5,
+  FaNodeJs,
+  FaNpm,
+  FaReact,
+} from 'react-icons/fa';
+import { TbBrandNextjs, TbBrandRedux, TbBrandVite } from 'react-icons/tb';
+import { BsGit } from 'react-icons/bs';
+import {
+  SiAdobe,
+  SiCanva,
+  SiExpress,
+  SiJavascript,
+  SiPostgresql,
+  SiPostman,
+  SiPowerbi,
+  SiTailwindcss,
+  SiMongodb,
+  SiTypescript,
+} from 'react-icons/si';
+import { DiPhp } from 'react-icons/di';
+import { FaLaravel } from 'react-icons/fa';
 
 const skillsData = [
-  { name: "HTML", icon: FaHtml5, color: "#F16529", category: "Frontend" },
-  { name: "CSS", icon: FaCss3Alt, color: "#2965F1", category: "Frontend" },
-  { name: "React Js", icon: FaReact, color: "#61DBFB", category: "Frontend" },
-  { name: "Redux", icon: TbBrandRedux, color: "#764ABC", category: "Frontend" },
-  { name: "Next Js", icon: TbBrandNextjs, color: "#454545", category: "Frontend" },
-  { name: "Bootstrap", icon: FaBootstrap, color: "#7952B3", category: "Frontend" },
-  { name: "Tailwind", icon: SiTailwindcss, color: "#38B2AC", category: "Frontend" },
-  { name: "Vite Js", icon: TbBrandVite, color: "rgb(139,109,244,1)", category: "Frontend" },
-  { name: "PostgreSQL", icon: SiPostgresql, color: "#336791", category: "Backend" },
-  { name: "Express.js", icon: SiExpress, color: "#454545", category: "Backend" },
-  { name: "Node Js", icon: FaNodeJs, color: "#339933", category: "Backend" },
-  { name: "NPM", icon: FaNpm, color: "#CB3837", category: "Backend" },
-  { name: "Adobe", icon: SiAdobe, color: "#FF0000", category: "Design" },
-  { name: "Figma", icon: FaFigma, color: "#F24E1E", category: "Design" },
-  { name: "Canva", icon: SiCanva, color: "#4ababc", category: "Design" },
-  { name: "Docker", icon: FaDocker, color: "#2496ED", category: "DevOps" },
-  { name: "Postman", icon: SiPostman, color: "#FF6C37", category: "DevOps" },
-  { name: "Git", icon: BsGit, color: "#F05032", category: "Version Control" },
-  { name: "Power BI", icon: SiPowerbi, color: "#F2C811", category: "Data Visualization" },
-  { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E", category: "Programming Languages" },
-  { name: "TypeScript", icon: SiTypescript, color: "#3178C6", category: "Programming Languages" },
+  { name: 'HTML', icon: FaHtml5, color: '#F16529', category: 'Frontend' },
+  { name: 'CSS', icon: FaCss3Alt, color: '#2965F1', category: 'Frontend' },
+  { name: 'React Js', icon: FaReact, color: '#61DBFB', category: 'Frontend' },
+  { name: 'Redux', icon: TbBrandRedux, color: '#764ABC', category: 'Frontend' },
+  {
+    name: 'Next Js',
+    icon: TbBrandNextjs,
+    color: '#454545',
+    category: 'Frontend',
+  },
+  {
+    name: 'Bootstrap',
+    icon: FaBootstrap,
+    color: '#7952B3',
+    category: 'Frontend',
+  },
+  {
+    name: 'Tailwind',
+    icon: SiTailwindcss,
+    color: '#38B2AC',
+    category: 'Frontend',
+  },
+  {
+    name: 'Vite Js',
+    icon: TbBrandVite,
+    color: 'rgb(139,109,244,1)',
+    category: 'Frontend',
+  },
+  {
+    name: 'PostgreSQL',
+    icon: SiPostgresql,
+    color: '#336791',
+    category: 'Backend',
+  },
+  {
+    name: 'Express.js',
+    icon: SiExpress,
+    color: '#454545',
+    category: 'Backend',
+  },
+  { name: 'Node Js', icon: FaNodeJs, color: '#339933', category: 'Backend' },
+  { name: 'NPM', icon: FaNpm, color: '#CB3837', category: 'Backend' },
+  { name: 'Adobe', icon: SiAdobe, color: '#FF0000', category: 'Design' },
+  { name: 'Figma', icon: FaFigma, color: '#F24E1E', category: 'Design' },
+  { name: 'Canva', icon: SiCanva, color: '#4ababc', category: 'Design' },
+  { name: 'Docker', icon: FaDocker, color: '#2496ED', category: 'DevOps' },
+  { name: 'Postman', icon: SiPostman, color: '#FF6C37', category: 'DevOps' },
+  { name: 'MongoDB', icon: SiMongodb, color: '#4DB33D', category: 'Backend' },
+  { name: 'Git', icon: BsGit, color: '#F05032', category: 'Version Control' },
+  {
+    name: 'Power BI',
+    icon: SiPowerbi,
+    color: '#F2C811',
+    category: 'Data Visualization',
+  },
+  {
+    name: 'JavaScript',
+    icon: SiJavascript,
+    color: '#F7DF1E',
+    category: 'Programming Languages',
+  },
+  {
+    name: 'TypeScript',
+    icon: SiTypescript,
+    color: '#3178C6',
+    category: 'Programming Languages',
+  },
+  {
+    name: 'PHP',
+    icon: DiPhp,
+    color: '#777BB4',
+    category: 'Programming Languages',
+  },
+  { name: 'Laravel', icon: FaLaravel, color: '#FF2D20', category: 'Backend' },
 ];
 
 export default skillsData;
