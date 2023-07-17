a243d6f38bfb9039df8f68e54041f0e93a8ed113
cereceda1991 <cereceda1991@gmail.com>
2023-07-16 21:53:03 -0500

    feat: add content languages

--- src/components/SliderTestimonials.jsx
+++ src/components/SliderTestimonials.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './styles/SliderTestimonials.css';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const SliderTestimonials = ({ testimonials }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderRef = useRef(null);
 
   const handleNextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide === testimonials.length - 1 ? 0 : prevSlide + 1));
@@ -13,30 +14,36 @@ const SliderTestimonials = ({ testimonials }) => {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? testimonials.length - 1 : prevSlide - 1));
   };
 
-  const testimonialSlides = testimonials.map((testimonial, index) => (
-    <div
-      key={testimonial.id}
-      className={`testimonial ${index === currentSlide ? 'active' : ''}`}
-    >
-      <img src={testimonial.photoUrl} alt={testimonial.name} className="testimonial__photo" />
-      <p>{testimonial.position}</p>
-      <h1>{testimonial.name}</h1>
-      <span>{testimonial.testimonial}</span>
-      <div className='container_testimonials-social'>
-        <a href={testimonial.linkedinUrl} target="_blank" rel="noopener noreferrer" className='social_linkedin'>
-          <FaLinkedin />
-        </a>
-        <a href={testimonial.githubUrl} target="_blank" rel="noopener noreferrer" className='social_FaGithub'>
-          <FaGithub />
-        </a>
-      </div>
-    </div>
-  ));
-
   return (
     <div className="slider_testimonials">
-      <div className="slider-container">
-        {testimonialSlides}
+      <div className="slider-container" ref={sliderRef}>
+        <div className="testimonial-slider">
+          {testimonials.map((testimonial, index) => {
+            const isActive = index === currentSlide;
+            const isPrevious = index === currentSlide - 1 || (currentSlide === 0 && index === testimonials.length - 1);
+            const isNext = index === currentSlide + 1 || (currentSlide === testimonials.length - 1 && index === 0);
+
+            return (
+              <div
+                key={testimonial.id}
+                className={`testimonial ${isActive ? 'active' : ''} ${isPrevious ? 'previous' : ''} ${isNext ? 'next' : ''}`}
+              >
+                <img src={testimonial.photoUrl} alt={testimonial.name} className="testimonial__photo" />
+                <p>{testimonial.position}</p>
+                <h1>{testimonial.name}</h1>
+                <span>{testimonial.testimonial}</span>
+                <div className="container_testimonials-social">
+                  <a href={testimonial.linkedinUrl} target="_blank" rel="noopener noreferrer" className="social_linkedin">
+                    <FaLinkedin />
+                  </a>
+                  <a href={testimonial.githubUrl} target="_blank" rel="noopener noreferrer" className="social_FaGithub">
+                    <FaGithub />
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <button className="slider-button prev" onClick={handlePrevSlide}>
         Prev
